c8069ab53453ed63b22d3a4843dc808b2dbc6e2a
Nick thompson <nickthompson@Nicks-Laptop.local>
2024-03-15 20:01:42 -0400

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,58 @@
-/*
-	Directive by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the modal
+  var modal = document.getElementById("myModal");
 
-(function($) {
+  // Get the button that opens the modal
+  var btn = document.getElementById("openModal");
 
-	var	$window = $(window),
-		$body = $('body');
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-	// Breakpoints.
-		breakpoints({
-			wide:      [ '1281px',  '1680px' ],
-			normal:    [ '981px',   '1280px' ],
-			narrow:    [ '841px',   '980px'  ],
-			narrower:  [ '737px',   '840px'  ],
-			mobile:    [ '481px',   '736px'  ],
-			mobilep:   [ null,      '480px'  ]
-		});
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+      modal.style.display = "block";
+  }
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
 
-})(jQuery);
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+
+  document.getElementById("quoteForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent the default form submission
+      
+      // Get form data
+      var selectedServices = document.querySelectorAll('input[name^="service"]:checked');
+      var servicesText = Array.from(selectedServices).map(function(checkbox) {
+          return checkbox.nextElementSibling.textContent.trim();
+      }).join(', ');
+      
+      var name = document.getElementById("name").value;
+      var phone = document.getElementById("phone").value;
+      var address = document.getElementById("address").value;
+      var details = document.getElementById("details").value;
+      
+      // Compose email body
+      var subject = "Quote Request";
+      var body = "Selected Services: " + servicesText + "\n"; // Line break for new line
+      body += "Name: " + name + "\n";
+      body += "Phone Number: " + phone + "\n";
+      body += "Address: " + address + "\n";
+      body += "Additional Details: " + details;
+      
+      // Construct mailto link
+      var mailtoLink = "mailto:nickrthompson94@gmail.com" +
+                       "?subject=" + encodeURIComponent(subject) +
+                       "&body=" + encodeURIComponent(body);
+      
+      // Open default email client with mailto link
+      window.location.href = mailtoLink;
+  });
+});
